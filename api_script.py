860b1604a287a495b6abbbdaf3c31724d2e5ab2b
TallTalha <talhaburakaydin.official@gmail.com>
2025-06-17 18:07:10 +0300

    fetch_stock_data funciton completed in api_script.py

--- api_script.py
+++ api_script.py
@@ -52,5 +52,40 @@ def fetch_stock_data(
     for attempt in range(1, MAX_RETIRES+1):
         try:
             response = requests.get(API_URL, params=params)
+            response.raise_for_status() # Raises an HTTPError for bad responses and then "expect block" to be executed
+            data = response.json() # Parses the JSON response
             
-        except
+            if 'Time Series' not in str(data):
+                if 'Error Message' in data:
+                    # Logs an error message if the API returns an error
+                    logger.error(f"api_script.py: Error fetching data for {symbol} -> {data['Error Message']}")
+                elif 'Note' in data:
+                    # Logs a warning if the API rate limit is exceeded
+                    logger.warning(f"api_script.py: API call limit reached -> {data['Note']}")
+                else:
+                    # Logs an error if the response does not contain expected data
+                    logger.error(f"api_script.py: Unexpected response format for {symbol} -> {data}")
+                return None
+            
+            # Gets dynamic key for the time series data
+            key = [k for k in data.json() if "Time Series" in k][0]
+            
+            # Converts the time series data to a DataFrame
+            df = pd.DataFrame.from_dict(data[key], orient='index', dtype=float)
+            df.index.name = 'timestamp'  # Sets the index name to 'timestamp'
+            print(df.columns) # Debugging line to check the columns of the DataFrame
+            
+            df.index = pd.to_datetime(df.index)  # Converts the index to datetime
+            df.sort_index(inplace=True)  # Sorts the DataFrame by index
+            return df  # Returns the DataFrame
+        except requests.exceptions.RequestException as e:
+            logging.error(f"api_script.py: Request failed for {symbol} on attempt {attempt}: {e}")
+            if attempt < MAX_RETIRES:
+                logger.info(f"api_script.py: Retrying in {RETRY_DELAY} seconds...")
+                time.sleep(RETRY_DELAY)
+            else:
+                logger.error(f"api_script.py: Max retries reached for {symbol}. Giving up.")
+                return None
+    return None  # Returns None if all attempts fail
+
+
