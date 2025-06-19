5fda560231a4b8f35bd7c094644f82363ccb9168
TallTalha <talhaburakaydin.official@gmail.com>
2025-06-19 15:24:58 +0300

    docker-compose.yaml and init.sql created

--- postgres_setup/init.sql
+++ postgres_setup/init.sql
@@ -1,3 +1,4 @@
+-- This script initializes the PostgreSQL database schema for a stock market data warehouse.
 CREATE TABLE dim_stocks (
     id SERIAL PRIMARY KEY,
     symbol VARCHAR(16) NOT NULL UNIQUE,
@@ -5,25 +6,24 @@ CREATE TABLE dim_stocks (
     sector VARCHAR(100),
     country VARCHAR(100),
     market_cap BIGINT,
-    last_updated TIMESTAMPZ DEFAULT NOW()
+    last_updated TIMESTAMPTZ DEFAULT NOW()
 );
 
-CREATE TABLE fact_stock_prices (
+CREATE TABLE fact_prices (
     stock_id INT NOT NULL,
     "timestamp" TIMESTAMPTZ NOT NULL,
     "open" NUMERIC(10, 4),
-    "high" NUMERIC(10, 4),
-    "low" NUMERIC(10, 4),
+    high NUMERIC(10, 4),
+    low NUMERIC(10, 4),
     "close" NUMERIC(10, 4),
     "volume" BIGINT,
 
     -- Composite primary key on stock_id and timestamp
-    CONSTRAINT pk_fact_stock_prices PRIMARY KEY (stock_id, "timestamp"), 
+    CONSTRAINT pk_fact_prices PRIMARY KEY (stock_id, "timestamp"),
 
-    -- Foreign key constraint to dim_stocks
+     -- Foreign key constraint to dim_stocks
     CONSTRAINT fk_dim_stocks FOREIGN KEY (stock_id) REFERENCES dim_stocks(id) 
-        ON DELETE CASCAE -- If a stock is deleted from dim_stocks, automatically delete related prices
-
+        ON DELETE CASCADE -- If a stock is deleted from dim_stocks, automatically delete related prices
 );
 
-CREATE INDEX idx_fact_stock_prices_timestamp ON fact_stock_prices("timestamp" DESC);
+CREATE INDEX idx_fact_prices_timestamp ON fact_prices("timestamp" DESC);
